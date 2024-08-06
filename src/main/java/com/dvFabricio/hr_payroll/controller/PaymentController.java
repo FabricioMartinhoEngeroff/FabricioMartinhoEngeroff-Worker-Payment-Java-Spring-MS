4b33299f0fd217225b15f06d638200e7291498eb
Fabricio <fa.engeroff1996@gmail.com>
2024-08-06 08:30:41 -0300

    Comunicação entre serviços payment e worker hard code pelo host

--- src/main/java/com/dvFabricio/hr_payroll/controller/PaymentController.java
+++ src/main/java/com/dvFabricio/hr_payroll/controller/PaymentController.java
@@ -17,7 +17,7 @@ public class PaymentController {
     private PaymentService service;
 
     @GetMapping(value = "/{workerId}/days/{days}")
-    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) throws Exception {
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
--- src/main/java/com/dvFabricio/hr_payroll/services/PaymentService.java
+++ src/main/java/com/dvFabricio/hr_payroll/services/PaymentService.java
@@ -1,13 +1,36 @@
 package com.dvFabricio.hr_payroll.services;
 
 import com.dvFabricio.hr_payroll.entities.Payment;
+import com.dvFabricio.hr_payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(Long workerId, int days){
-        return  new Payment("bob", 200.0, days);
+    @Value("${hr-worker.host}")
+    private String workerHost;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(Long workerId, int days) throws Exception {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        if (worker == null) {
+
+            throw new Exception("Worker not found with ID: " + workerId);
+        }
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
+
 }
