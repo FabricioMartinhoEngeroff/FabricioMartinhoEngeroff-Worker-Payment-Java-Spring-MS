abfa4cacb7a520cabb1ef75e300c88091b0b4ec5
Fabricio <fa.engeroff1996@gmail.com>
2024-08-06 09:02:25 -0300

    Feign client

---
+++ src/main/java/com/dvFabricio/hr_payroll/feignClient/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package com.dvFabricio.hr_payroll.feignClient;
+
+import com.dvFabricio.hr_payroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "worker-service", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+    @GetMapping(value = "/{id}")
+     ResponseEntity<Worker> findById(@PathVariable Long id);
+
+
+}
--- src/main/java/com/dvFabricio/hr_payroll/services/PaymentService.java
+++ src/main/java/com/dvFabricio/hr_payroll/services/PaymentService.java
@@ -2,29 +2,19 @@ package com.dvFabricio.hr_payroll.services;
 
 import com.dvFabricio.hr_payroll.entities.Payment;
 import com.dvFabricio.hr_payroll.entities.Worker;
+import com.dvFabricio.hr_payroll.feignClient.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long workerId, int days) throws Exception {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
 
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         if (worker == null) {
 
             throw new Exception("Worker not found with ID: " + workerId);
